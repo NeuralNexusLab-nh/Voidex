a1d75180a747816658b8334a655c5bc57cd96629
Specter Panther <neuralnexuslab@hotmail.com>
2026-03-20 17:59:10 +0800

    Create ip.php

---
+++ ip.php
@@ -0,0 +1,26 @@
+<?php
+function getIP() {
+    $ipKeys = [
+        'HTTP_CLIENT_IP',
+        'HTTP_X_FORWARDED_FOR',
+        'HTTP_X_FORWARDED',
+        'HTTP_X_CLUSTER_CLIENT_IP',
+        'HTTP_FORWARDED_FOR',
+        'HTTP_FORWARDED',
+        'REMOTE_ADDR'
+    ];
+
+    foreach ($ipKeys as $key) {
+        if (!empty($_SERVER[$key])) {
+            $ips = explode(',', $_SERVER[$key]);
+            foreach ($ips as $ip) {
+                $ip = trim($ip);
+                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+                    return $ip;
+                }
+            }
+        }
+    }
+    return 'UNKNOWN';
+}
+echo getIP();
